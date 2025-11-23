3e456156f458eedf71a658932bf2504a54b3102e
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-23 14:23:52 +0400

    Add App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+function App() {
+  return (
+    <div className="app">
+      <h1>Fitness Tracker Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+}
+export default App;
