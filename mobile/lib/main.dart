7bbd54680bf4be8d80be36fb4dfeb5379a49d028
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-23 14:23:54 +0400

    Add main.dart

---
+++ mobile/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'screens/dashboard_screen.dart';
+void main() {
+  runApp(const FitnessApp());
+}
+class FitnessApp extends StatelessWidget {
+  const FitnessApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness Tracker',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const DashboardScreen(),
+    );
+  }
+}
