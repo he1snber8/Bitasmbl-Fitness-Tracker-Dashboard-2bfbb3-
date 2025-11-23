92a3ebdbccb3b30f85cbf32bd5369d3fc9272ad7
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-23 14:23:47 +0400

    Add server.js

---
+++ backend/src/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', routes);
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`API running on ${port}`));
